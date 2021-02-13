8b3c2830d72a472462693df17f2b9bf4d15f377c
Jahnavi <kathulajahnavi100@gmail.com>
2021-02-13 17:28:29 +0530

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import logo from './logo.png';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
             <div className="container-fluid">
-                <a className="navbar-brand ml-0" href="#">
-                    <img src='../assests/logo.png' style={{ width: '260px' }} /></a>
+                <a className="navbar-brand ml-0" href="http://localhost:3000/">
+                    <img src={logo} alt="logo " style={{ width: '260px' }} /></a>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
